509b9380ade33f0c317d0adeb7db518a89df4afd
Vivek Krishnan <vrkrishn@Viveks-MacBook-Pro-2.local>
2014-01-15 11:52:12 -0500

    added analytics

--- steves_files/index.php
+++ steves_files/index.php
@@ -26,5 +26,15 @@
   <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
   <script src="dist/js/bootstrap.min.js"></script>
   <script src="dist/js/holder.js"></script>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-47177860-1', 'phideltcmu.com');
+    ga('send', 'pageview');
+
+  </script>
 </body>
 </html>
